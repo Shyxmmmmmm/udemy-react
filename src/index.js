f6c054564794d334dedceb0b01de9783ae7d5b80
shyxmmmmmm <shyamkumar2005464@gmail.com>
2026-03-24 13:10:21 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from "react";
 import ReactDom from 'react-dom/client'
-import './index.css'
-import Header from './components/Header'
-import Section2 from './components/Section2'
-import Section3 from './components/Section3'
-import Section4 from './components/Section4'
-import Section5 from './components/Section5'
-import Section6 from './components/Section6'
-import Footer from "./components/Footer";
-
+import App from "./App";
 const a=ReactDom.createRoot(document.getElementById("root"))
 
 a.render(
   <div>
-    <Header/>
-    <Section2/>
-    <Section3/>
-    <Section4/>
-    <Section5/>
-    <Section6/>
-    <Footer/>
+    <App></App>
   </div>
 )
